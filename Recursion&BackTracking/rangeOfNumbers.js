05fa4ed121ce6ed52ba795207548648e65ada8e9
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-09-14 22:07:53 +0530

    added code

--- Recursion&BackTracking/rangeOfNumbers.js
+++ Recursion&BackTracking/rangeOfNumbers.js
@@ -1,5 +1,11 @@
+let arr = []
 function range(start, end) {
-    
+    if(start<end){
+        arr.push(start)
+        return range(++start, end)
+    }
+    arr.push(end)
+    return arr;
 }
 
 console.log(range(5, 11));
