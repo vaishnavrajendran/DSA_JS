e25e859faac4c758585f3429c4789da5cd8130c4
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-09-17 22:23:24 +0530

    backtracking

--- Recursion&BackTracking/reverseString.js
+++ Recursion&BackTracking/reverseString.js
@@ -6,4 +6,12 @@ function reverse(str){
     let reversed = popped.concat(reverse(split))
     return reversed;
 } 
+
+/* 2nd Approach */
+
+function reverse(str) {
+    if(str.length === 1) return str;
+    return reverse(str.substr(1)) + str.charAt(0)
+}
+
 console.log(reverse('helloo'));
