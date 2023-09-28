ae317253f14368e3a7f4b907d328c843c767ad02
vaishnavrajendran <vaishnavrajendran95@gmail.com>
2023-09-28 21:34:48 +0530

    splitting

--- Arrays/Searching/BinarySearch/splitArrayLargestSum.js
+++ Arrays/Searching/BinarySearch/splitArrayLargestSum.js
@@ -1,5 +1,5 @@
 /*
-410. Split Array Largest Sum  [Hard]
+410. Split Array Largest Sum  [Hard-GOOGLE]
 Given an integer array nums and an integer k, split nums into k non-empty subarrays such that the largest sum of any subarray is minimized.
 Return the minimized largest sum of the split.
 A subarray is a contiguous part of the array.
